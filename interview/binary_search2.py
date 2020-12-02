311672001be17f17bc97e7ea5be6161d793eb915
china-testing <x_r_zh@163.com>
2020-12-02 16:39:48 +0800

    modify binary_search2.py

--- interview/binary_search2.py
+++ interview/binary_search2.py
@@ -3,18 +3,18 @@
 # 技术支持：dwz.cn/qkEfX1u0 项目实战讨论QQ群630011153 144081101
 # CreateDate: 2019-12-29
 
-def bin_search(items,lower=0, upper=None, item=0):
-    if upper is None: 
-        upper = len(items) - 1
-        
-    if lower == upper:
-        return upper
+def bin_search(l,low,high,item):
+    if low > high:
+        return None
     else:
-        middle = (lower + upper) // 2
-        if item > items[middle]:
-            return bin_search(items, middle + 1, upper,item)
+        mid = (low + high) // 2
+        value = l[mid]
+        if value == item:
+            return mid
+        elif value > item:
+            return bin_search(l, low, high-1, item)
         else:
-            return bin_search(items, lower, middle,item)
+            return bin_search(l, mid+1, high, item)
 
 if __name__ == '__main__':
     l = list(range(1,30,3)) # [1, 4, 7, 10, 13, 16, 19, 22, 25, 28]
