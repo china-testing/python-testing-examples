3df3c2fa36569b69395a302b30cf9207c71f1efa
china-testing <x_r_zh@163.com>
2020-12-02 16:38:21 +0800

    modify binary_search2.py

--- interview/binary_search2.py
+++ interview/binary_search2.py
@@ -21,3 +21,4 @@ if __name__ == '__main__':
     print(bin_search(l,0,len(l)-1, 7)) # return 2
     print(bin_search(l,0,len(l)-1, 1)) # return 0 
     print(bin_search(l,0,len(l)-1, 29)) # return None
+
