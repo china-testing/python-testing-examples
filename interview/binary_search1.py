16873e86befe2e78d2a9dc1eef59bebeb102c141
china-testing <x_r_zh@163.com>
2020-12-03 16:50:23 +0800

    Add bubble_sort*.py

--- interview/binary_search1.py
+++ interview/binary_search1.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 技术支持：dwz.cn/qkEfX1u0 项目实战讨论QQ群630011153 144081101
+# 技术支持：dwz.cn/qkEfX1u0 项目实战讨论Q群144081101 书籍Q群6089740
 # CreateDate: 2019-12-29
 
 
-def binary_search(items, item):
+def bin_search(items, item):
     low = 0
     high = len(items) - 1
     while low <= high:
@@ -19,6 +19,7 @@ def binary_search(items, item):
     return None
 
 if __name__ == '__main__':
-    l = list(range(1,100,3))    
-    print(binary_search(l, 31)) 
-    print(binary_search(l, 30)) 
+    l = list(range(1,30,3)) # [1, 4, 7, 10, 13, 16, 19, 22, 25, 28]
+    print(bin_search(l, 7)) # return 2
+    print(bin_search(l, 1)) # return 0 
+    print(bin_search(l, 29)) # return None
---
+++ interview/bubble_sort2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 技术支持：dwz.cn/qkEfX1u0 项目实战讨论Q群144081101 书籍Q群6089740
+# CreateDate: 2019-12-29
+
+'''冒泡排序经典python面试题'''
+def bubble_sort(l):
+    n = len(l)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if l[j] > l[j+1]:
+                l[j],l[j+1] = l[j+1],l[j]
+            
+if __name__ == '__main__':
+    l = [21,6,9,33,3] 
+    bubble_sort(l)
+    print(l) # return [3, 6, 9, 21, 33]
